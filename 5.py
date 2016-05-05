746b2e5a65b05944f751f4c1288c756a26cae4bb
Karl <karlsmith@bouzou.com>
2016-05-04 22:51:27 -0400

    refactors 5.py

--- 5.py
+++ 5.py
@@ -1,28 +1,35 @@
-import sys
+from tqdm import trange
 
 testUntil = 1048576
 EvenlyDivisibleBy = 10
-testRange = range(1, testUntil+1)
-debug = False
 
 
-def my_num(myVar):
-    outputNum = 1
-    for iter in range(1, min(len(str(bin(myVar)))-2, EvenlyDivisibleBy)+1):
-        if bin(myVar)[len(str(bin(myVar)))-iter] == str(1):
-            outputNum *= (iter+1)
-    if bin(myVar)[len(str(bin(myVar))) - 1] == str(1): sys.stdout.write('.')
-    return outputNum
+def binary_digit_to_encoded_factor(digitToUnencode):
+    encodedFactor = 1
+    lengthOfBinaryString = len(str(bin(digitToUnencode)))-2
+    for iter in range(1, lengthOfBinaryString+1):
+        if binary_number_has_1_in_spot(digitToUnencode,iter):
+            encodedFactor *= (iter+1)
+    return encodedFactor
 
 
-for eachNum in testRange:
+def binary_number_has_1_in_spot(binaryNum, spot):
+    return bin(binaryNum)[len(str(bin(binaryNum))) - spot] == str(1)
+
+
+def the_answer_is(answer):
+    print "\nAnswer:"
+    print answer
+    print "is evenly divisible by " + str(EvenlyDivisibleBy)
+    print "as well as every number lower"
+    quit()
+
+
+for eachNum in trange(1, testUntil+1):
     for eachDivisor in range(1, EvenlyDivisibleBy+1):
-        if my_num(eachNum) % eachDivisor != 0:
+        if binary_digit_to_encoded_factor(eachNum) % eachDivisor != 0:
             break
         elif eachDivisor == EvenlyDivisibleBy:
-            print "\nAnswer:"
-            print my_num(eachNum)
-            print "is evenly divisible by " + str(EvenlyDivisibleBy)
-            print "as well as every number lower"
-            quit()
+            the_answer_is(binary_digit_to_encoded_factor(eachNum))
     pass
+print "No answer found"
