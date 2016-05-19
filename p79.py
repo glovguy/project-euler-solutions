49d73366c5c1b7f20cfc252e17422db39ccde8e3
Karl <karlsmith@bouzou.com>
2016-05-19 00:17:11 -0400

    Adds DigitPair class and changes Fragment class to be DigitSeries. Moving towards a better implementation, wip.

--- p79.py
+++ p79.py
@@ -7,7 +7,8 @@ for line in f:
     keylog.append(int(line))
 
 
-class fragment(object):
+class DigitSeries(object):
+    """Understands a series of digits"""
     def __init__(self, digits):
         self.nums = digits
         self.digits = str(digits)
@@ -17,26 +18,52 @@ class fragment(object):
         for currenti in range(0, 3):
             for eachi in range(0, 3):
                 if self.digits[currenti] == other.digits[eachi]:
-                    commonDigitsSet.append([currenti, eachi])
+                    commonDigitsSet.append([DigitPair(currenti, eachi)])
         return commonDigitsSet
+
+    def slice(self, digitPair):
+        return DigitSeries(self.digits[:digitPair.a])
 
     def possible_combinations(self, other):
         commonDigitSet = self.common_digits(other)
-        return [int(self.digits[:commonDigitSet[0][0]] + other.digits[:commonDigitSet[0][1]] +
-            self.digits[commonDigitSet[0][0]] +
-            self.digits[commonDigitSet[0][0]+1:] + other.digits[commonDigitSet[0][1]+1:])]
+        combinations = []
+        numCombinationsBefore = len(self.digits[:commonDigitSet[0][0]]) + len(other.digits[:commonDigitSet[0][1]])
+        for x in range(numCombinationsBefore):
+            beforeSharedDigit = self.digits[:commonDigitSet[0][x]] + other.digits[:commonDigitSet[0][x+1]]
+            sharedDigit = self.digits[commonDigitSet[0][0]]
+            afterSharedDigit = self.digits[commonDigitSet[0][0]+1:] + other.digits[commonDigitSet[0][1]+1:]
+            combinations.append(int(beforeSharedDigit+sharedDigit+afterSharedDigit))
+        return combinations
+
+
+class DigitPair(object):
+    """Understands a pair of digits"""
+
+    def __init__(self, first, second):
+        self.a = first
+        self.b = second
+
+    def __eq__(self, other):
+        return (self.a == other.a and self.b == other.b) or \
+               (self.a == other.b and self.b == other.a)
+
+# def is_compatible(firstCommonDigit, secondCommonDigit):
+#     if firstCommonDigit[0] == secondCommonDigit[0] or firstCommonDigit[1] == secondCommonDigit[1]:
+#         return False
+#     firstSlope = firstCommonDigit[1] - firstCommonDigit[0]
+#     secondSlope = secondCommonDigit[1] - secondCommonDigit[0]
 
 
 class test_functions(unittest.TestCase):
 
     def test_does_simplify(self):
-        self.assertEqual(fragment(729).common_digits(fragment(316)), [])
-        self.assertEqual(fragment(620).common_digits(fragment(762)), [[0, 1], [1, 2]])
-        self.assertEqual(fragment(129).common_digits(fragment(620)), [[1, 1]])
+        self.assertEqual(DigitSeries(729).common_digits(DigitSeries(316)), [])
+        self.assertEqual(DigitSeries(620).common_digits(DigitSeries(762)), [[DigitPair(0, 1)], [DigitPair(1, 2)]])
+        self.assertEqual(DigitSeries(129).common_digits(DigitSeries(620)), [[DigitPair(1, 1)]])
 
     def test_possible_combinations(self):
-        self.assertEqual(fragment(123).possible_combinations(fragment(300)), [12300])
-        self.assertEqual(fragment(123).possible_combinations(fragment(200)), [12003, 12030, 12300])
+        self.assertEqual(DigitSeries(123).possible_combinations(DigitSeries(300)), [12300])
+        self.assertEqual(DigitSeries(123).possible_combinations(DigitSeries(200)), [12003, 12030, 12300])
 
 
 if __name__ == '__main__':
